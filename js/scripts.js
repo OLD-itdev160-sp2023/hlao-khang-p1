e185d2d15afa4d2062a324ca434cd066dac81a31
unknown <khangh@gmatc.matc.edu>
2023-04-27 17:45:32 -0500

    Completed Project 1

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var salutation = 'Hello';
+var user = 'Hlao';
+var greeting = salutation + ', ' + user + '!';
+var greetingEl = document.getElementById('greeting')
+
+greetingEl.textContent = greeting;
